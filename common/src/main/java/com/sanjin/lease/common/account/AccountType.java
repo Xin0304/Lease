16f83bda9d9c6af426ab8ee389ee3b174cd89629
sanjin <2914451501@qq.com>
2026-03-27 19:58:04 +0800

    Changes

---
+++ common/src/main/java/com/sanjin/lease/common/account/AccountType.java
@@ -0,0 +1,6 @@
+package com.sanjin.lease.common.account;
+
+public class AccountType {
+    public static final String ADMIN = "admin"; // 管理后台账号类型
+    public static final String APP = "app";     // 移动端租客账号类型
+}
